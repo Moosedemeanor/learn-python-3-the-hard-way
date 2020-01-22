bf75da28439e92963536acaddd2a80d9a1b330d0
Moosedemeanor <charlesdavidbouley@gmail.com>
2020-01-22 16:26:56 -0500

    LP3THW: 2020-01-22

--- ex01.py
+++ ex01.py
@@ -5,7 +5,7 @@ print("This is fun")
 print('Yay! Printing')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
-#study drills
+# study drills
 print("I'm printing another line")
 # comment out all 'print' commands except one to print just one line
 # octothorpe comments out lines in Python
---
+++ ex02.py
@@ -0,0 +1,14 @@
+# a comment, this is so you can read your program later.
+# anything after the # is ignored by Python.
+
+print("I could have code like this.") # and the comment after is ignored
+
+# you can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+# study drills
+# i was correct about commenting out code
+# reviewed backwards
+# no additional mistakes
+# read outloud; no additional mistakes
