298dc2642543efd1dfd12bd177679e9d42c67077
Moosedemeanor <charlesdavidbouley@gmail.com>
2020-01-24 16:32:50 -0500

    LP3THW: 2020-01-24

---
+++ ex03.py
@@ -0,0 +1,39 @@
+# numbers and math
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+# prints a string asking a question
+print("I will now count my chickens:")
+# prints the string 'Hens' then performs 30/6 which is 5, then adds 5 to 25
+print("Hens", 25 + 30 / 6)
+# prints the string 'Roosers' then performs 100 - 25 * 3 % 4
+print("Roosters", 100 - 25 * 3 % 4)
+# prints a string
+print("Now I will count the eggs:")
+# performs a calculation in PEMDAS order
+# 4 % 2 = 0 | 1 / 4 = 0.25
+# 3 + 2 + 1 - 5 + 0 - 0.25 + 6
+# 3 + 2 + 1 = 6
+# 6 + 0 - 0.25 + 6 = 11.75
+# 11.75 - 5 = 6.75
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# prints a string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
