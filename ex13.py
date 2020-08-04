78c0f0c98dfb5ad56181a63768107ff88ed94ac3
Moosedemeanor <charlesdavidbouley@gmail.com>
2020-08-04 15:36:41 -0400

    LP3THW: 2020-08-04

--- ex13.py
+++ ex13.py
@@ -1,7 +1,8 @@
 # import argv from the sys module
 from sys import argv
 # read the wyss section for how to run this
-script, first, second, third = argv
+script, first, second = argv
+third=input("Please type a third input: ")
 
 print("The script is called:", script)
 print("Your first variable is:", first)
