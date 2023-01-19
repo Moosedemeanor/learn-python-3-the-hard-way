c6ee8dbec6cfec23c5c2e8e8efb0edba915e73cf
Moosedemeanor <Moosedemeanor@users.noreply.github.com>
2023-01-19 18:08:19 -0500

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,38 @@
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+# print string text question
+print("I will now count my chickens:")
+# print Hens string then perform calculation
+print("Hens", 25 + 30 / 6)
+# print Roosters string then perform calculation
+print("Roosers", 100 - 25 * 3 % 4)
+# print string statement
+print("Now I will count the eggs:")
+# print the result of the calculation
+# PEMDAS - Parentheses Exponents Multiplication Division Addition Subtraction
+print(3 + 2 + 1 - 5 + 4 % 2 - 1.00 / 4 + 6)
+# print string question
+print("Is it true that 3 + 2 < 5 -7?")
+# perform the actual calculation
+print(3 + 2 < 5 -7)
+# print string question, perform addition
+print("What is 3 + 2?", 3 + 2)
+# print string question, perform subtraction
+print("What is 5 - 7?", 5 - 7)
+# print string question
+print("Oh, that's what it's False.")
+# print string question
+print("How about some more.")
+# print string, perform boolean operation
+print("Is it greater?", 5 > -2 )
+# print string, perform boolean operation
+print("Is is greater or equal?", 5 >= -2)
+# print string, perform boolean operation
+print("Is it less or equal?", 5 <= -2)
